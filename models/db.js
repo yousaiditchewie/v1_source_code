0a58430561780edf2da00f12fb9648f7dcb563f3
Jacob Allen <jedijake@me.com>
2017-07-16 14:31:15 -0700

    create db database and users table

--- models/db.js
+++ models/db.js
@@ -3,9 +3,6 @@ var url = "mongodb://localhost:27017/homeGrownDB";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  db.createCollection("users", function(err, res) {
-    if (err) throw err;
-    console.log("Table created!");
-    db.close();
-  });
+  console.log("Database created!");
+  db.close();
 });
